f770a8fd03551e4164a7a8c3c71aa58b81cde91a
Wouter Thielen <wouter@morannon.org>
2014-09-26 23:08:30 +0900

    Implemented stocksplits
    Also improved download routine for easier stocksplit management, and
    fixed a bug where it would skip the first line of the matrix in the xts
    object creation.

--- getSymbols.yahooj.R
+++ getSymbols.yahooj.R
@@ -13,11 +13,11 @@
         }
         if(!exists("adjust", environment(), inherits=FALSE))
             adjust <- FALSE
-        
+
         default.return.class <- return.class
         default.from <- from
         default.to <- to
-        
+
         if(missing(verbose)) verbose <- FALSE
         if(missing(auto.assign)) auto.assign <- TRUE
         yahoo.URL <- "http://info.finance.yahoo.co.jp/history/"
@@ -29,21 +29,20 @@
             from <- if(is.null(from)) default.from else from
             to <- getSymbolLookup()[[Symbols[[i]]]]$to
             to <- if(is.null(to)) default.to else to
-            
+
             from.y <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][1])
             from.m <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][2])
             from.d <- as.numeric(strsplit(as.character(as.Date(from,origin='1970-01-01')),'-',)[[1]][3])
             to.y <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][1])
             to.m <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][2])
             to.d <- as.numeric(strsplit(as.character(as.Date(to,origin='1970-01-01')),'-',)[[1]][3])
-            
+
             Symbols.name <- getSymbolLookup()[[Symbols[[i]]]]$name
             Symbols.name <- ifelse(is.null(Symbols.name),Symbols[[i]],Symbols.name)
             if(verbose) cat("downloading ",Symbols.name,".....\n\n")
-            
-            cols <- c('Open','High','Low','Close','Volume','Adjusted')
-            mat <- matrix(0, ncol=7, nrow=0, byrow=TRUE)
+
             page <- 1
+            totalrows <- c()
             while (TRUE) {
                 tmp <- tempfile()
                 download.file(paste(yahoo.URL,
@@ -57,38 +56,57 @@
                                     "&tm=d",
                                     "&p=",page,
                                     sep=''),destfile=tmp,quiet=!verbose)
-                
+
                 fdoc <- htmlParse(tmp)
                 unlink(tmp)
-                
+
                 rows <- xpathApply(fdoc, "//table[@class='boardFin yjSt marB6']//tr")
                 if (length(rows) == 1) break
-                
-                for(row in rows) {
-                    cells <- getNodeSet(row, "td")
-                    if (length(cells) != 7) next
-                    
-                    date <- as.Date(xmlValue(cells[[1]]), format="%Y年%m月%d日")
-                    entry <- c(date)
-                    for(n in 2:length(cells)) {
-                        entry <- cbind(entry, as.numeric(gsub(",", "", xmlValue(cells[[n]]))))
-                    }
-                    mat <- rbind(mat, entry)
-                }
+
+                totalrows <- c(totalrows, rows)
                 page <- page + 1
             }
             if(verbose) cat("done.\n")
-            
-            fr <- xts(mat[-1, -1], as.Date(mat[-1, 1]), src="yahooj", updated=Sys.time())
+
+            # Process from the start, for easier stocksplit management
+            totalrows <- rev(totalrows)
+            mat <- matrix(0, ncol=7, nrow=0, byrow=TRUE)
+            factor <- 1
+            for(row in totalrows) {
+                cells <- getNodeSet(row, "td")
+
+                # 2 cells means it is a stocksplit row
+                # So extract stocksplit data and recalculate the matrix we have so far
+                if (length(cells) == 2) {
+                    ss.data <- as.numeric(na.omit(as.numeric(unlist(strsplit(xmlValue(cells[[2]]), "[^0-9]+")))))
+                    factor <- factor * ss.data[2] / ss.data[1]
+
+                    mat <- rbind(t(apply(mat[-nrow(mat),], 1, function(x) {
+                        x * c(1, rep(1/factor, 4), factor, 1)
+                    })), mat[nrow(mat),])
+                }
+
+                if (length(cells) != 7) next
+
+                date <- as.Date(xmlValue(cells[[1]]), format="%Y年%m月%d日")
+                entry <- c(date)
+                for(n in 2:length(cells)) {
+                    entry <- cbind(entry, as.numeric(gsub(",", "", xmlValue(cells[[n]]))))
+                }
+
+                mat <- rbind(mat, entry)
+            }
+
+            fr <- xts(mat[, -1], as.Date(mat[, 1]), src="yahooj", updated=Sys.time())
             symname <- paste('YJ', toupper(Symbols.name), sep="")
             colnames(fr) <- paste(symname,
                                   c('Open','High','Low','Close','Volume','Adjusted'),
                                   sep='.')
-            
+
             fr <- convert.time.series(fr=fr,return.class=return.class)
             if(is.xts(fr))
                 indexClass(fr) <- index.class
-            
+
             Symbols[[i]] <- symname
             if(auto.assign)
                 assign(Symbols[[i]],fr,env)
@@ -96,6 +114,7 @@
                 message("pausing 1 second between requests for more than 5 symbols")
                 Sys.sleep(1)
             }
+
         }
         if(auto.assign)
             return(Symbols)
